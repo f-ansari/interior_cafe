dc43416c9ef9ee52035adf51080534688a7f400e
Faryal Ansari <faryalansari@live.com>
2021-04-12 17:51:03 -0400

    [models => user.py] create table

---
+++ models/user.py
@@ -0,0 +1,17 @@
+from models.db import db
+from datetime import datetime
+from sqlalchemy.orm import joinedload
+
+
+class User(db.Model):
+    __tablename__ = 'users'
+
+    id = db.Column(db.Integer, primary_key=True)
+    first_name = db.Column(db.String(20), nullable=False)
+    last_name = db.Column(db.String(20), nullable=False)
+    username = db.Column(db.String(30), nullable=False)
+    email = db.Column(db.String(20), nullable=False)
+    password_digest = db.Column(db.String(255), nullable=False)
+    created_at = db.Column(
+        db.DateTime, default=datetime.utcnow, nullable=False)
+    updated_at = db.Column(db.DateTime default=datetime.utcnow, nullable=False, onupdate=datetime.now())

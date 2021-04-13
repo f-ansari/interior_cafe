059d7bbfac9ec4dec2aa20357cd3bbe1b4096b2b
Faryal Ansari <faryalansari@live.com>
2021-04-12 21:27:42 -0400

    [models => post.py] updated table

--- models/post.py
+++ models/post.py
@@ -6,10 +6,11 @@ from sqlalchemy.orm import joinedload
 class Post(db.Model):
     __tablename__ = 'posts'
 
-    id = db.Coloumn(db.Integer, primary_key=True)
-    title = db.Coloumn(db.String(255), nullable=False)
-    description = db.Coloumn(db.String(500), nullable=False)
-    like = db.Coloumn(db.Integer, nullable=False)
+    id = db.Column(db.Integer, primary_key=True)
+    post_id = db.Column(db.Integer, db.ForeignKey(users.id), nullable=False)
+    title = db.Column(db.String(255), nullable=False)
+    description = db.Column(db.String(500), nullable=False)
+    like = db.Column(db.Integer, nullable=False)
     created_at = db.Column(
         db.DateTime, default=datetime.utcnow, nullable=False)
     updated_at = db.Column(db.DateTime, default=date.utcnow,

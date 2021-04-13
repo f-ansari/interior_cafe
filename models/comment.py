2f2e4e7da8f8fc3774dbdda9f9d1f4abe4979e11
Faryal Ansari <faryalansari@live.com>
2021-04-12 21:33:14 -0400

    [models => updated table w/ associations

--- models/comment.py
+++ models/comment.py
@@ -7,10 +7,14 @@ class Comment(db.Model):
     __tablename__ = 'comments'
 
     id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('users.id'), nullable=False)
+    post_id = db.Column(db.Integer, db.ForeignKey('posts.id'), nullable=False)
     comment = db.Column(db.String(255), nullable=False)
     created_at = db.Column(
         db.DateTime, default=datetime.utcnow, nullable=False)
     updated_at = db.Column(db.DateTime, default=datetime.utcnow,
                            nullable=False, onupdate=datetime.now())
+    user = sb.relationship(
+        "User", backref=db.backref('comments_user', lazy=True))
     post = db.relationship(
         "Post", backref=db.backref('comment_post', lazy=True))

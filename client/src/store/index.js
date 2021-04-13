b5d0db9bb20d48f27f5426959fdb800001116f3d
Faryal Ansari <faryalansari@live.com>
2021-04-13 17:20:24 -0400

    [auth reduce] initial set up

---
+++ client/src/store/index.js
@@ -0,0 +1,12 @@
+import { createStore, combineReducers, applyMiddleware } from 'redux'
+import thunk from 'redux-thunk'
+import { composeWithDevTools } from 'redux-devtools-extension'
+
+const store = createStore(
+  combineReducers({
+    //   hello
+  }),
+  composeWithDevTools(applyMiddleware(thunk))
+)
+
+export default store
---
+++ client/src/store/type.js
@@ -0,0 +1,9 @@
+// global types
+export const ADD_TO_FRONTEND = 'ADD_TO_BACKEND'
+
+// login section
+export const FIRST_NAME = 'FIRST_NAME'
+export const LAST_NAME = 'LAST_NAME'
+export const USERNAME = 'USERNAME'
+export const USER_EMAIL = 'USER_EMAIL'
+export const PASSWORD = 'PASSWORD'
